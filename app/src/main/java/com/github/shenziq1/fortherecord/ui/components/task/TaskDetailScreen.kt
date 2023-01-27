9059430f8dcd95cb6357ad3cbecd99df2f2c7561
shenziq2 <zshen331@gatech.edu>
2023-01-27 17:11:39 -0500

    simple 1 min countdown

--- app/src/main/java/com/github/shenziq1/fortherecord/ui/components/task/TaskDetailScreen.kt
+++ app/src/main/java/com/github/shenziq1/fortherecord/ui/components/task/TaskDetailScreen.kt
@@ -1,51 +1,71 @@
 package com.github.shenziq1.fortherecord.ui.components.task
 
-import android.util.Log
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalFocusManager
-import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
-import com.github.shenziq1.fortherecord.ui.common.TestText
 import com.github.shenziq1.fortherecord.ui.common.TopBackBar
 import com.github.shenziq1.fortherecord.viewmodel.TaskDetailViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 
-@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun TaskDetailScreen(
     id: Int,
     navHostController: NavHostController,
     viewModel: TaskDetailViewModel = hiltViewModel()
 ) {
-    val focusManager = LocalFocusManager.current
     val coroutineScope = rememberCoroutineScope()
-    val keyboardController = LocalSoftwareKeyboardController.current
     val taskUiState = viewModel.taskUiState.collectAsState()
-    Log.d("viewmodel2", taskUiState.value.name)
+    var timeLeft by remember {
+        mutableStateOf(60000L)
+    }
+    var isRunning by remember {
+        mutableStateOf(false)
+    }
 
-//    coroutineScope.launch {
-//        viewModel.getUiState(id)
-//    }
-    val name = taskUiState.value.name
-    Scaffold(topBar = { TopBackBar(onClick = {navHostController.popBackStack()}) }) {
+    Scaffold(topBar = { TopBackBar(onClick = { navHostController.popBackStack() }) }) {
         Column(
             modifier = Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceEvenly
         ) {
-            TestText(text = name)
-//            Button(onClick = {
-//                //focusManager.moveFocus(FocusDirection.Down)
-//                navHostController.navigate("TaskEdit/${id}")
-//            }) {
-//                Text(text = "edit")
-//            }
+            Text(text = taskUiState.value.name)
+            Text(text = (timeLeft/1000L).toString(), fontSize = 36.sp)
+            FloatingActionButton(onClick = {
+                when(isRunning){
+                    true -> {
+                        isRunning = false
+                    }
+                    false -> {
+                        isRunning = true
+                        coroutineScope.launch {
+                            while (timeLeft > 0L && isRunning){
+                                delay(100L)
+                                timeLeft -= 100L
+                            }
+                            isRunning = false
+                        }
+                    }
+                }
+
+            }, modifier = Modifier.size(48.dp)) {
+                when(isRunning){
+                    true -> Text(text = "Stop")
+                    false -> Text(text = "Start")
+                }
+
+                //Icon(imageVector = Icons.Default.Settings, contentDescription = "")
+            }
+
+
 //            Button(onClick = { coroutineScope.launch {
 //                viewModel.deleteTask()
 //                navHostController.popBackStack()
